51b5d7d44eea599a79760bbbf4cd987e99e8d333
aMa2210 <tkm199888@gmail.com>
2024-10-23 20:23:08 +0800

    Fix bugs

--- CalculateAccuracy.py
+++ CalculateAccuracy.py
@@ -9,6 +9,7 @@ column3 = 'answer_fine-tuned_temperature:0'
 column4 = 'answer_fine-tuned_temperature:0ftby1056data'
 column5 = 'answer_original_temperature:0_promptNoRandomHint'
 column6 = 'answer_fine-tuned_temperature:0_promptNoRandomHint'
+column7 = 'answer_fine-tuned3k_temperature:0_promptNoRandomHint'
 
 
 equal_values_ori = df[column1] == df[column2]
@@ -16,6 +17,7 @@ equal_values_ft = df[column1] == df[column3]
 equal_values_ftby1056 = df[column1] == df[column4]
 equal_values_ori_noHint = df[column1] == df[column5]
 equal_values_ft_noHint = df[column1] == df[column6]
+equal_values_ft3k_noHint = df[column1] == df[column7]
 # for index, row in df.iterrows():
 #     if row[column2] != row[column3]:
 #         print(str(index)+': '+'ori: '+str(row[column2])+'  ft: '+str(row[column3]))
@@ -25,6 +27,10 @@ count_ft = equal_values_ft.sum()
 count_ftby1056 = equal_values_ftby1056.sum()
 count_ori_noHint = equal_values_ori_noHint.sum()
 count_ft_noHint = equal_values_ft_noHint.sum()
+count_ft3k_noHint = equal_values_ft3k_noHint.sum()
+
+
+num_minus1_ft3k_noHint = sum(df[column7] == -1)
 
 
 prob_ori = count_ori / len(df)
@@ -32,6 +38,8 @@ prob_ft = count_ft / len(df)
 prob_ftby1056 = count_ftby1056 / len(df)
 prob_ori_noHint = count_ori_noHint / 2398
 prob_ft_noHint = count_ft_noHint / 2399
+prob_ft3k_noHint = count_ft3k_noHint / (len(df)-num_minus1_ft3k_noHint)
+
 
 
 print('correct num of original model: '+str(count_ori))
@@ -39,12 +47,14 @@ print('correct num of finetuned model: '+str(count_ft))
 print('correct num of finetunedby1056 model: '+str(count_ftby1056))
 print('correct num of ori_noHint model: '+str(count_ori_noHint))
 print('correct num of ft_noHint model: '+str(count_ft_noHint))
+print('correct num of ft3k_noHint model: '+str(count_ft3k_noHint))
 
 print('correct probability of original model: '+str(prob_ori))
 print('correct probability of finetuned model: '+str(prob_ft))
 print('correct probability of finetunedby1056 model: '+str(prob_ftby1056))
 print('correct probability of ori_noHint model: '+str(prob_ori_noHint))
 print('correct probability of ft_noHint model: '+str(prob_ft_noHint))
+print('correct probability of ft3k_noHint model: '+str(prob_ft3k_noHint))
 
 
 # Acc-H
@@ -72,3 +82,13 @@ for i in range(num_groups):
         # print(i)
         count_all_true_groups += 1
 print('acc-h num(ft): ', count_all_true_groups)
+
+count_all_true_groups = 0
+for i in range(num_groups):
+    start_idx = i * group_size
+    end_idx = start_idx + group_size
+    group = equal_values_ft3k_noHint[start_idx:end_idx]
+    if group.all():
+        # print(i)
+        count_all_true_groups += 1
+print('acc-h num(ft3k): ', count_all_true_groups)
--- CalculateProbOfResults.py
+++ CalculateProbOfResults.py
@@ -4,27 +4,30 @@ import pandas as pd
 # df = pd.read_csv('result/FinetuneData_combination_withoutAnswer_result.csv')
 # df = pd.read_csv('result/combination_correct_options_result_temperature0.csv')
 # df = pd.read_csv('result/combination_correct_options_result_temperature0.csv', encoding='ISO-8859-1')
-df = pd.read_csv('result/Dataset_3k_randomOptions_result_temperature0.csv', encoding='ISO-8859-1')
+# df = pd.read_csv('result/Dataset_3k_randomOptions_result_temperature0.csv', encoding='ISO-8859-1')
+df = pd.read_csv('result/combination_correct_options_result_temperature0_ftBy3kData.csv', encoding='ISO-8859-1')
 
 
-column_values = df['answer_original_temperature:0']
-value_counts = column_values.value_counts(normalize=True)
-value_counts_num = column_values.value_counts(normalize=False)
-selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
-selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
-print(selected_values)
-print(selected_values_num)
-print(sum(selected_values_num))
-
-column_values = df['answer_fine-tuned_temperature:0']
-value_counts = column_values.value_counts(normalize=True)
-value_counts_num = column_values.value_counts(normalize=False)
-selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
-selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
-print(selected_values)
-print(selected_values_num)
-print(sum(selected_values_num))
+# column_values = df['answer_original_temperature:0']
+# value_counts = column_values.value_counts(normalize=True)
+# value_counts_num = column_values.value_counts(normalize=False)
+# selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
+# selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
+# print(selected_values)
+# # print(selected_values_num)
+# print(sum(selected_values_num))
 #
+# column_values = df['answer_fine-tuned_temperature:0']
+# value_counts = column_values.value_counts(normalize=True)
+# value_counts_num = column_values.value_counts(normalize=False)
+# selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
+# selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
+# print(selected_values)
+# # print(selected_values_num)
+# print(sum(selected_values_num))
+
+
+
 # column_values = df['answer_fine-tuned_temperature:0ftby1056data']
 # value_counts = column_values.value_counts(normalize=True)
 # value_counts_num = column_values.value_counts(normalize=False)
@@ -34,20 +37,23 @@ print(sum(selected_values_num))
 # # print(selected_values_num)
 #
 #
-# column_values = df['answer_original_temperature:0_promptNoRandomHint']
-# value_counts = column_values.value_counts(normalize=True)
-# value_counts_num = column_values.value_counts(normalize=False)
-# selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
-# selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
-# print(selected_values)
-# print(selected_values_num)
-#
-#
-#
-# column_values = df['answer_fine-tuned_temperature:0_promptNoRandomHint']
-# value_counts = column_values.value_counts(normalize=True)
-# value_counts_num = column_values.value_counts(normalize=False)
-# selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
-# selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
-# print(selected_values)
-# print(selected_values_num)
+
+column_values = df['answer_original_temperature:0_promptNoRandomHint']
+column_values = column_values[column_values != -1]
+value_counts = column_values.value_counts(normalize=True)
+value_counts_num = column_values.value_counts(normalize=False)
+selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
+selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
+print(selected_values)
+print(sum(selected_values_num))
+
+
+
+column_values = df['answer_fine-tuned_temperature:0_promptNoRandomHint']
+column_values = column_values[column_values != -1]
+value_counts = column_values.value_counts(normalize=True)
+value_counts_num = column_values.value_counts(normalize=False)
+selected_values = value_counts.reindex([0, 1, 2, 3], fill_value=-1)
+selected_values_num = value_counts_num.reindex([0, 1, 2, 3], fill_value=-1)
+print(selected_values)
+print(sum(selected_values_num))
